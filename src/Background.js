21c503f778845cff1cd6353dc16aa473efbfe14f
narekb8 <nab176@pitt.edu>
2025-05-05 01:39:54 -0400

    Take 1 Deploy React app to GitHub Pages

--- src/Background.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-export default function Background() {
-  return (
-    <video className="videoTag" autoPlay loop muted>
-        <source src='/videos/background-video.mp4' type='video/mp4' />
-    </video>
-  );
-}
